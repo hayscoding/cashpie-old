e5153b758bcca3607e8247fbf870e2dc06965d40
HaysS <stanfordhays@gmail.com>
2018-09-12 00:26:20 -0500

    changed to 4x4 grid with wider top row. Looks great

--- App.js
+++ App.js
@@ -15,26 +15,32 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.row}>
+        <View style={styles.topRow}>
           <Text>Entered Numbers Shown Here</Text>        
         </View> 
         <View style={styles.row}>
-          <View style={styles.box}><Text style={styles.buttonText}>Box1</Text></View>
-          <View style={styles.box}><Text style={styles.buttonText}>Box1</Text></View>
-          <View style={styles.box}><Text style={styles.buttonText}>Box1</Text></View>
+          <View style={styles.box}><Text style={styles.buttonText}>Clr</Text></View>
+          <View style={styles.box}><Text style={styles.buttonText}>penile</Text></View>
+          <View style={styles.box}><Text style={styles.buttonText}>3</Text></View>
+          <View style={styles.box}><Text style={styles.buttonText}>4</Text></View>
+        </View> 
+        <View style={styles.row}>
+          <View style={styles.box}><Text style={styles.buttonText}>7</Text></View>
+          <View style={styles.box}><Text style={styles.buttonText}>8</Text></View>
+          <View style={styles.box}><Text style={styles.buttonText}>9</Text></View>
           <View style={styles.box}><Text style={styles.buttonText}>Box1</Text></View>
         </View> 
         <View style={styles.row}>
-          <View style={styles.box}><Text style={styles.buttonText}>Box1</Text></View>
-          <View style={styles.box}><Text style={styles.buttonText}>Box1</Text></View>
-          <View style={styles.box}><Text style={styles.buttonText}>Box1</Text></View>
+          <View style={styles.box}><Text style={styles.buttonText}>4</Text></View>
+          <View style={styles.box}><Text style={styles.buttonText}>5</Text></View>
+          <View style={styles.box}><Text style={styles.buttonText}>6</Text></View>
           <View style={styles.box}><Text style={styles.buttonText}>Box1</Text></View>
         </View> 
         <View style={styles.row}>
-          <View style={styles.box}><Text style={styles.buttonText}>Box1</Text></View>
-          <View style={styles.box}><Text style={styles.buttonText}>Box1</Text></View>
-          <View style={styles.box}><Text style={styles.buttonText}>Box1</Text></View>
-          <View style={styles.box}><Text style={styles.buttonText}>Box1</Text></View>
+          <View style={styles.box}><Text style={styles.buttonText}>1</Text></View>
+          <View style={styles.box}><Text style={styles.buttonText}>2</Text></View>
+          <View style={styles.box}><Text style={styles.buttonText}>3</Text></View>
+          <View style={styles.box}><Text style={styles.buttonText}>0</Text></View>
         </View> 
 
       </View>
@@ -46,20 +52,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  topRow: {
+    flex: 2,
+    flexDirection: 'row',
+  },
   row: {
     flex: 1,
     flexDirection: 'row',
-    justifyContent: 'flex-start',
   },
   box: {
     borderWidth: 2,
+    flexDirection: 'column',
     borderColor: 'black',
     height: height,
     width: box_width,
     backgroundColor: '#CECCCF',
+    alignItems: "center"
   },
   buttonText: {
-    textAlign: 'center',
-    justifyContent: 'space-around',
+    flex: 1,
   },
 });
