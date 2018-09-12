333a212370404ef89f94b4b5230044332728a0dd
HaysS <stanfordhays@gmail.com>
2018-09-11 23:39:54 -0500

    Created grid of views

--- App.js
+++ App.js
@@ -1,11 +1,36 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  View,
+  Dimensions,
+  Text
+} from 'react-native';
+ 
+var { height, width } = Dimensions.get('window');
+ 
+var box_count = 3;
+var box_width = width / box_count;
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <View style={styles.row}>
+          <View style={[styles.box, styles.box1]}><Text>Box1</Text></View>
+          <View style={[styles.box, styles.box2]}><Text>Box2</Text></View>
+          <View style={[styles.box, styles.box3]}><Text>Box3</Text></View>
+        </View> 
+        <View style={styles.row}>
+          <View style={[styles.box, styles.box1]}><Text>Box1</Text></View>
+          <View style={[styles.box, styles.box2]}><Text>Box2 </Text></View>
+          <View style={[styles.box, styles.box3]}><Text>Box3</Text></View>
+        </View> 
+        <View style={styles.row}>
+          <View style={[styles.box, styles.box1]}><Text>Box1</Text></View>
+          <View style={[styles.box, styles.box2]}><Text>Box2</Text></View>
+          <View style={[styles.box, styles.box3]}><Text>Box3</Text></View>
+        </View> 
+
       </View>
     );
   }
@@ -14,8 +39,25 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    borderBottomWidth: 1,
+    borderBottomColor: '#fff'
+  },
+  box: {
+    height: height,
+    width: box_width,
+  },
+  box1: {
+    backgroundColor: '#2196F3'
+  },
+  box2: {
+    backgroundColor: '#8BC34A'
+  },
+  box3: {
+    backgroundColor: '#e3aa1a'
+  }
 });
